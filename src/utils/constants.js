7106cce2c58239d220f605307b69dd8090f8da6e
Arman Eskandari <arman_eskandari@live.com>
2021-04-16 10:06:47 +0430

    setting up the Navbar

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const links = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "products",
+    url: "/products",
+  },
+];
