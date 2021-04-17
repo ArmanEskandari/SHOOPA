1e48f48042860ff7f1408e8b79a992883184dcc9
Arman Eskandari <arman_eskandari@live.com>
2021-04-18 00:17:00 +0430

    setting up Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from "react";
+import styled from "styled-components";
+import { services } from "../utils/constants";
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom styles
+            <br />
+            style your shoes
+          </h3>
+          <p>
+            rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt
+            lobortis feugiat vivamus at augue eget arcu dictum varius duis at
+            consectetur
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article key={id} className="service">
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +97,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiCompass, GiDiamondHard, GiStabbedNote } from "react-icons/gi";
 
 export const links = [
   {
@@ -15,5 +16,29 @@ export const links = [
     id: 3,
     text: "products",
     url: "/products",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCompass />,
+    title: "mission",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard />,
+    title: "vision",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote />,
+    title: "history",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
   },
 ];
