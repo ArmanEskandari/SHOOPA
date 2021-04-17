b9d915c028863d69248afd0becec3450a3962810
Arman Eskandari <arman_eskandari@live.com>
2021-04-18 01:38:32 +0430

    fetching products from api

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,9 +1,21 @@
 import React, { useContext, useEffect, useReducer } from "react";
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import axios from "axios";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from "../actions";
 import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featuerd_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -20,6 +32,23 @@ export const ProductsProvider = ({ children }) => {
     console.log("closed");
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      dispatch({type:GET_PRODUCTS_ERROR})
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,4 +1,10 @@
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from "../actions";
 
 const products_reducer = (state, action) => {
   switch (action.type) {
@@ -7,6 +13,25 @@ const products_reducer = (state, action) => {
 
     case SIDEBAR_CLOSE:
       return { ...state, isSidebarOpen: false };
+
+    case GET_PRODUCTS_BEGIN:
+      return { ...state, products_loading: true };
+
+    case GET_PRODUCTS_SUCCESS: {
+      const featuerd_products = action.payload.filter(
+        (product) => product.featured === true
+      );
+      return {
+        ...state,
+        products_loading: false,
+        products: action.payload,
+        featuerd_products,
+      };
+    }
+
+    case GET_PRODUCTS_ERROR:
+      return { ...state, products_loading: false, products_error: true };
+
     default:
       return state;
   }
