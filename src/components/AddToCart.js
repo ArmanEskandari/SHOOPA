23870973ba85ee31b7b98d1ccc9230c413f8e7f6
Arman Eskandari <arman_eskandari@live.com>
2021-04-18 17:57:53 +0430

    AddToCart - colors

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -3,8 +3,33 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>;
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+
+  const [mainColor, setMainColor] = useState(colors[0]);
+
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span> colors : </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                className={`${
+                  mainColor === color ? "color-btn active" : "color-btn"
+                }`}
+                style={{ background: `${color}` }}
+                onClick={() => setMainColor(color)}>
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
