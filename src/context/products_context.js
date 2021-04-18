4e4fa4613cba64e5d87666a3e4a3e0f7b0bb14c8
Arman Eskandari <arman_eskandari@live.com>
2021-04-18 11:16:06 +0430

    fetching single product data

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -6,6 +6,9 @@ import {
   GET_PRODUCTS_BEGIN,
   GET_PRODUCTS_SUCCESS,
   GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions";
 import reducer from "../reducers/products_reducer";
 import { products_url as url } from "../utils/constants";
@@ -16,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -42,13 +48,26 @@ export const ProductsProvider = ({ children }) => {
       console.log(response);
     } catch (error) {
       console.log(error);
-      dispatch({type:GET_PRODUCTS_ERROR})
+      dispatch({ type: GET_PRODUCTS_ERROR });
     }
   };
 
   useEffect(() => {
     fetchProducts(url);
   }, []);
+
+  const fetchSingleProducts = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -4,6 +4,9 @@ import {
   GET_PRODUCTS_BEGIN,
   GET_PRODUCTS_SUCCESS,
   GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions";
 
 const products_reducer = (state, action) => {
@@ -31,6 +34,23 @@ const products_reducer = (state, action) => {
 
     case GET_PRODUCTS_ERROR:
       return { ...state, products_loading: false, products_error: true };
+
+    case GET_SINGLE_PRODUCT_BEGIN:
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
+
+    case GET_SINGLE_PRODUCT_SUCCESS:
+      return { ...state, single_product: action.payload };
+
+    case GET_SINGLE_PRODUCT_ERROR:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product_error: true,
+      };
 
     default:
       return state;
