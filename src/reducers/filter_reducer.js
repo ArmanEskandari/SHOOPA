b8f1555f7793367795495ac0efdef3c8a48c6b33
Arman Eskandari <arman_eskandari@live.com>
2021-04-23 13:52:55 +0430

    Filters - default values - max_price, price

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -12,10 +12,13 @@ import {
 const filter_reducer = (state, action) => {
   switch (action.type) {
     case LOAD_PRODUCTS:
+      let maxPrice = action.payload.map((product) => product.price);
+      maxPrice = Math.max(...maxPrice) * 1000;
       return {
         ...state,
         all_products: [...action.payload],
         filtered_products: [...action.payload],
+        filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
       };
 
     case SET_GRIDVIEW:
