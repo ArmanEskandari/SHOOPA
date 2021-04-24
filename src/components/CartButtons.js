cad949af7524e9f9f0678f7d04938784553da1cc
Arman Eskandari <arman_eskandari@live.com>
2021-04-25 00:40:10 +0430

    calculating Total price

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -3,17 +3,19 @@ import styled from "styled-components";
 import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
+import { useCartContext } from "../context/cart_context";
 
 const CartButtons = () => {
   const data = useProductsContext();
   const { closeSidebar } = data;
+  const { total_items } = useCartContext();
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={() => closeSidebar()}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">0</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       <button type="button" className="auth-btn">
