dab49522b627929d38bfe8318b659de4f3f8cf16
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 14:03:12 +0430

    Filters - shipping checkbox

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -72,6 +72,10 @@ export const FilterProvider = ({ children }) => {
       value = Number(value);
     }
 
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
+
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
 
