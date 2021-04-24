1516b7de0976d61d999840555eb2100ebc8d922d
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 12:03:24 +0430

    Filters - categories

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -41,6 +41,26 @@ const Filters = () => {
             />
           </div>
           {/* end search input */}
+
+          {/* categories */}
+          <div className="form-control">
+            <h5>category</h5>
+            <div>
+              {categories.map((c, index) => (
+                <button
+                  key={index}
+                  onClick={updateFilters}
+                  type="button"
+                  name="category"
+                  className={`${
+                    category === c.toLowerCase() ? "active" : null
+                  }`}>
+                  {c}
+                </button>
+              ))}
+            </div>
+          </div>
+          {/* end of categories */}
         </form>
       </div>
     </Wrapper>
