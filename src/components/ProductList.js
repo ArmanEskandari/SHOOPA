9fde2d7ec2db348e802c4c9a42b7519f52923020
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 14:40:06 +0430

    fixed typo

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,7 +6,7 @@ import ListView from "./ListView";
 const ProductList = () => {
   const { filtered_products: products, grid_view } = useFilterContext();
   if (products.length < 1) {
-    return <h5>Sorry, no products matched your serach...</h5>;
+    return <h5>Sorry, no products matched your search...</h5>;
   }
   if (grid_view === false) {
     return <ListView products={products} />;
