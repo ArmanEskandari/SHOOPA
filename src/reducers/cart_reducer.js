cf26d80089a852b19661e7ab495af40b147dd49a
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 23:59:52 +0430

    remove item and clear cart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -36,7 +36,15 @@ const cart_reducer = (state, action) => {
         };
         return { ...state, cart: [...state.cart, newItem] };
       }
-      return;
+    }
+
+    case REMOVE_CART_ITEM: {
+      const tempCart = state.cart.filter((item) => item.id !== action.payload);
+      return { ...state, cart: tempCart };
+    }
+
+    case CLEAR_CART: {
+      return { ...state, cart: [] };
     }
 
     default:
