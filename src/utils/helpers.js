b757dd9205c24f59f50cbed294501078338538e9
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 11:31:07 +0430

    Filters - unique values

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,3 +5,11 @@ export const formatPrice = (number) => {
   }).format(number * 1000);
   return newNumber;
 };
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+  return ["all", ...new Set(unique)];
+};
