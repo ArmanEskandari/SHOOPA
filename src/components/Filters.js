79f6c59724727939ea9bcb0c507220daf888d8e0
Arman Eskandari <arman_eskandari@live.com>
2021-04-24 13:01:46 +0430

    Filter - price

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -115,6 +115,20 @@ const Filters = () => {
             </div>
           </div>
           {/* end of colors */}
+
+          {/* price */}
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input
+              type="range"
+              name="price"
+              min={min_price}
+              max={max_price}
+              value={price}
+              onChange={updateFilters}></input>
+          </div>
+          {/* end of price */}
         </form>
       </div>
     </Wrapper>
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -68,6 +68,10 @@ export const FilterProvider = ({ children }) => {
       value = e.target.dataset.color;
     }
 
+    if (name === "price") {
+      value = Number(value);
+    }
+
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
 
